b070699939806f54708eccf1ca3e0b02ebffbfb5
Jeffrey Sinicki <jeffrey.sinicki@gmail.com>
2022-11-03 12:26:20 -0400

    enabled pylint and black code formatter in python

--- api/main.py
+++ api/main.py
@@ -19,18 +19,16 @@ CORS(app)
 
 app.config["DEBUG"] = DEBUG
 
+
 @app.route("/new-image")
 def new_image():
-    word = request.args.get("query")    
-    headers = {
-        "Accept-Version": "v1",
-        "Authorization": "Client-ID " + UNSPLASH_KEY
-    }    
-    params = {"query": word}    
+    word = request.args.get("query")
+    headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
+    params = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
-    
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
