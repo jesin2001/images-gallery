6ae16e126e22d34ba1f7fa9bb67d013397d327c1
Jeffrey Sinicki <jeffrey.sinicki@gmail.com>
2022-11-03 10:43:24 -0400

    changed api in the frontend and enabled cors in api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Welcome from "./components/Welcome";
 import { Container, Row, Col } from "react-bootstrap";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5050'
 
 const App = () => {
   const [word, setWord] = useState("");
@@ -16,7 +17,7 @@ const App = () => {
     e.preventDefault();
 
     fetch(
-      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+      `${API_URL}/new-image?query=${word}`
     )
       .then((res) => res.json())
       .then((data) => {
